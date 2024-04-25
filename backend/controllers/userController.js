de66a1950642d9499a3abd87e636deb221b067c4
pushkar1995 <pushkar.gautam1994@gmail.com>
2024-04-25 15:52:03 +0545

    Logging Users In using JWT Token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,6 +7,19 @@ const createToken = (_id) => {
 
 // login a user
 const loginUser = async (req, res) => {
+  const {email, password} = req.body
+
+  try {
+    const user = await User.login(email, password)
+
+    // Create  a token
+    const token = createToken(user._id)
+
+    res.status(200).json({email, token})
+  } catch (error)  {
+    res.status(400).json({error: error.message})
+  }
+
   res.json({mssg: 'login user'})
 }
 
@@ -21,7 +34,7 @@ const signupUser = async (req, res) => {
     const token = createToken(user._id)
 
     res.status(200).json({email, token})
-  } catch (error) {
+  } catch (error)  {
     res.status(400).json({error: error.message})
   }
 }
